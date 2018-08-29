05c6d26295d0711485c7edd5eba8b45f203f0960
ShiverMeTimbers <michelle.nyamai@gmail.com>
2018-08-29 14:43:04 +0100

    pin correction, add comparison

--- sketch_aug29a.ino
+++ sketch_aug29a.ino
@@ -10,7 +10,7 @@ int const  switch_A= 2;
 int const  switch_B= 3;
 int const  switch_C= 4;
 int const  switch_D= 5;
-int combination[4] ={4,2,3,1};
+int combination[4] ={5,3,4,2};
 int userInput[4];
 int serialoutput[4];
 
@@ -45,49 +45,55 @@ for(0;i<4;i++){
 
   //if pin is high!  
   if(A == HIGH){
-    Serial.println("Switch A working");
+    Serial.println("Switch A working \n");
     code = switch_A;
     userInput[i]=code;
     delay(1000);
   }
   
    else if(B == HIGH){
-    Serial.print("Switch B working");
+    Serial.print("Switch B working \n");
     code = switch_B;
     userInput[i]=code;
     delay(1000);
     
   }
   else if(C == HIGH){
-    Serial.println("Switch C working");
+    Serial.println("Switch C working \n");
     code = switch_C;
     userInput[i]=code;
     delay(1000);
     
   }
   else if(D == HIGH){
-    Serial.println("Switch D working");
+    Serial.println("Switch D working \n");
     code = switch_D;
     userInput[i]=code;
     delay(1000);
+    D=LOW;
+    digitalWrite(switch_D, LOW);  
     }
     //reset
-  A=LOW;B=LOW;C=LOW;D=LOW;  
-  digitalWrite(switch_A, LOW);
-  digitalWrite(switch_B, LOW);
-  digitalWrite(switch_C, LOW);
-  digitalWrite(switch_D, LOW);
+    A=LOW;B=LOW;C=LOW;
+    digitalWrite(switch_A, LOW);
+    digitalWrite(switch_B, LOW);
+    digitalWrite(switch_C, LOW);
+    
+    delay(1000);
   }//end for loop
 
   //display array
-  for(i=0;i<4;i++){  
-    Serial.print(userInput[i]);
-    delay(1000);
-  }
-  A=LOW;B=LOW;C=LOW;D=LOW;  
-  digitalWrite(switch_A, LOW);
-  digitalWrite(switch_B, LOW);
-  digitalWrite(switch_C, LOW);
-  digitalWrite(switch_D, LOW);
+//  for(i=0;i<4;i++){  
+//    Serial.print(userInput[i]);
+//    delay(1000);
+//  }
 
+  //compare pattern
+  for(i=0;i<4;i++){
+    if(combination[i] != userInput[i]){
+      Serial.print("Please try again!");delay(10000);
+     } 
+  } 
+  Serial.print("Unlocked!");delay(10000);
+     
 }
